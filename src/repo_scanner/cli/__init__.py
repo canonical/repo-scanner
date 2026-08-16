b1f7fbb350b28265e30a4b511c8500197d9fbe81
niwamo <nm@niwamo.com>
2026-08-15 19:10:49 -0500

    purge cli and config modules; to be re-implemented

--- src/repo_scanner/cli/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright 2026 Canonical Ltd.
-# See LICENSE file for licensing details.
-
-"""Command-line entry point for reposcan."""
-
-from repo_scanner.cli.main import main
-
-__all__ = ["main"]
